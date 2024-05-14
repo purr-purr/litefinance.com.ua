dc749f086def1a9d04babe29deba4147799e5101
pavelshaposhnikov <pashapasha2281@gmail.com>
2024-05-14 14:17:04 +0100

    fix: intro-component styled

--- src/modules/layout/components/Header/index.tsx
+++ src/modules/layout/components/Header/index.tsx
@@ -8,12 +8,26 @@ import {useMediaQuery} from '@modules/common/hooks';
 import {LAPTOP_BREAKPOINT} from '@utils/const';
 
 import s from './Header.module.scss';
+import {useEffect, useState} from "react";
+import cn from "classnames";
 
 const Header = () => {
 	const isLaptop = useMediaQuery(LAPTOP_BREAKPOINT);
+	const [scrollTop, setScrollTop] = useState<number>(0);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			setScrollTop(window.scrollY);
+		};
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
 	return (
-		<header className={s.container}>
+		<header className={cn(s.container, scrollTop > 100 && s.active)}>
 			<Logo/>
 
 			<HeaderContextWrapper>
